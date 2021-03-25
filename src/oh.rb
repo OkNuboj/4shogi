bc40eed38788da92cccdff26648779ada003d6e1
OkNuboj <oknuboj@gmail.com>
2021-03-25 16:14:38 +0900

    微修正

--- src/oh.rb
+++ src/oh.rb
@@ -6,9 +6,9 @@ end
 
 oh1 = Oh.new
 oh1.id = 8001
-oh1.place = [, ]
-oh1.owner = 
+oh1.place = [1, 5]
+oh1.owner = 1
 oh2 = Oh.new
 oh2.id = 8002
-oh2.place = [, ]
-oh2.owner = 
+oh2.place = [9, 5]
+oh2.owner = 2
